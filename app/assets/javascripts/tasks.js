f0f27226bffefefca830b247bb44f8f2d0c9632a
erica lai <ericalai89@gmail.com>
2016-08-07 20:11:47 +0000

    added strikethrough to finished tasks

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,11 +1,12 @@
 $(function() {
-
     // The taskHtml method takes in a JavaScript representation
     // of the task and produces an HTML representation using
     // <li> tags
     function taskHtml(task) {
       var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+      var liClass = task.done ? "completed" : "";
+      var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+      '<div class="view"><input class="toggle" type="checkbox"' +
         " data-id='" + task.id + "'" +
         checkedStatus +
         '><label>' +
@@ -22,16 +23,20 @@ $(function() {
     function toggleTask(e) {
       var itemId = $(e.target).data("id");
 
-
       var doneValue = Boolean($(e.target).is(':checked'));
-
 
       $.post("/tasks/" + itemId, {
         _method: "PUT",
         task: {
           done: doneValue
         }
-      });
+      }).success(function(data) {
+        var liHtml = taskHtml(data);
+        var $li = $("#listItem-" + data.id);
+        $li.replaceWith(liHtml);
+        $('.toggle').change(toggleTask);
+
+      } );
     }
 
     $.get("/tasks").success( function( data ) {
@@ -47,6 +52,7 @@ $(function() {
 
     });
 
+
     $('#new-form').submit(function(event) {
       event.preventDefault();
       var textbox = $('.new-todo');
@@ -60,7 +66,7 @@ $(function() {
         var ulTodos = $('.todo-list');
         ulTodos.append(htmlString);
         $('.toggle').click(toggleTask);
-        #('.new-todo').val('');
       });
     });
+
   });
